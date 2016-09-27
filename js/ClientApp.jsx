e9af31eb87ae7b032cf09c63b0f9ace6e18978f9
Phillip Jones <phillipemersonjones@gmail.com>
2016-09-27 12:24:59 -0500

    first commit

---
+++ js/ClientApp.jsx
@@ -0,0 +1,11 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+
+const App = () => (
+  <div className='App_conatainer'>
+    <h1>Hello</h1>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('app'))
